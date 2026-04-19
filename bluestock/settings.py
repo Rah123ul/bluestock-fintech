f3a91cc6d5bd8b9c7e16f1ce72fb7db3d51b3de9
Rahul kumar singh <rahul_b231186pe@itc.ac.in>
2026-04-19 20:41:25 +0530

    Add DATABASE_URL support

--- bluestock/settings.py
+++ bluestock/settings.py
@@ -80,3 +80,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
 }
+
+import dj_database_url
+if os.getenv('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.parse(os.getenv('DATABASE_URL'))
